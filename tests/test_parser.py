375eefe2833b055ea3e601662c7c2910f4e19818
Usdmal <ggg.qqq.1911@gmail.com>
2026-09-05 02:07:38 +0300

    Update pyproject.toml for tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,11 +1,7 @@
 import os
-import sys
 
 import pytest
 
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
-)
 from tree_to_excel import generate_output_path, parse_tree_file, save_to_excel
 
 
